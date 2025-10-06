7c4433293012ac6552a5cc767b4590d5351471b7
Yalin-Duan <yd2848@columbia.edu>
2025-10-05 22:53:19 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -2,26 +2,15 @@
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
 
-    def _norm_rate(r):
-        r = float(r)
-        return r / 100.0 if r >= 1.0 else r
+    n = int(m * ppy)               
+    r = y / ppy                   
+    c = face * couponRate / ppy    
 
-    y = _norm_rate(y)
-    c = _norm_rate(couponRate)
-    ppy = int(ppy)
-
-    n = int(round(float(m) * ppy))    
-    r = y / ppy                        
-    coupon = float(face) * c / ppy    
-
-    price = 0.0
+    price = 0
     for t in range(1, n + 1):
-        price += coupon / ((1.0 + r) ** t)
-    price += float(face) / ((1.0 + r) ** n)
+        price += c / (1 + r) ** t 
+    
+    price += face / (1 + r) ** n 
 
-    return int(round(price))
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+    return price
+    
