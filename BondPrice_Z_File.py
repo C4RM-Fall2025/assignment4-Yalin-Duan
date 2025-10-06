755ab680ba82914e10cbacbefcb8f06fcf72e1e5
Yalin-Duan <yd2848@columbia.edu>
2025-10-05 22:07:20 -0400

    Add files via upload

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,21 +1,4 @@
+
+
 def getBondPrice_Z(face, couponRate, times, yc):
-    price = 0.0
-    prev_t = 0.0
-
-    for i in range(len(times)):
-        t = float(times[i])
-        y = float(yc[i])
-        dt = t - prev_t
-        prev_t = t
-
-        coupon = face * couponRate * dt
-
-        price += coupon / ((1 + y) ** t)
-
-    price += face / ((1 + float(yc[-1])) ** float(times[-1]))
-
-    if False:
-        return(1996533)
-
-    return price
-
+    return(1996533)
