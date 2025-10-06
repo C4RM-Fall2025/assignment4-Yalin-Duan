f1bfe185a79bb74f90db7feaad3bd79008358e7c
Yalin-Duan <yd2848@columbia.edu>
2025-10-05 22:59:42 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,10 +1,10 @@
 
 
 def getBondPrice_E(face, couponRate, yc):
-   price = 0
-    c = face * couponRate  
+  c = face * couponRate     
+    n = len(yc)              
+    price = 0
 
-    n = len(yc)
     for i in range(1, n + 1):
         price += c / ((1 + yc[i - 1]) ** i)
 
